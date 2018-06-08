8e392580e348a00b7c4d067b478d2e14f25b4970
KerimBo <kerim.bojadzija@gmail.com>
2018-06-08 10:11:06 -0500

    Adding js, html, and css files
    Preliminary files

---
+++ Kerim.js
@@ -0,0 +1,13 @@
+function changeFirstP(){
+	document.getElementById("firstP").innerHTML = "DELETED HEHEHE";
+}
+
+function alertHello(){
+	window.alert("Hello!");
+}
+
+var myNum = 0;
+function increment(){
+	document.getElementById("numText").innerHTML = myNum;
+	myNum = myNum + 1;
+}
